d508225fa450278548f1e857e4109c338abcf6b4
sidmk07 <83899677+sidmk07@users.noreply.github.com>
2021-05-10 13:55:09 +0530

    Add files via upload

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        } 
+
+
+        /* Setting End-points for CRUD */
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = _context.Users.ToListAsync();
+            return await users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = _context.Users.FindAsync(id);
+            return await user;
+        }
+
+    }
+}
